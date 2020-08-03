42d7530d8bb969448718f88833c94386cf2db6fe
Vitiokas44 <russ25081989@gmail.com>
2020-08-03 09:30:27 -0500

    add intr.java

--- src/intr.java
+++ src/intr.java
@@ -2,6 +2,7 @@ public class intr {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("iiiiiii");
+        System.out.println("to git");
         
     }
 }
